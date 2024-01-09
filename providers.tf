4754c9d498539bc710a56d27c4d2e059049f24d3
Lateefat Arogundade <61961512+LateefahA@users.noreply.github.com>
2024-01-09 21:06:19 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-west-2"
-  shared_config_files      = ["/home/lateefat/.aws/config"]
-  shared_credentials_files = ["/home/lateefat/.aws/credentials"]
-  profile                  = "vscode"
+  shared_config_files      = ["path_to_config_file"]
+  shared_credentials_files = ["path_to_credentials"]
+  profile                  = "profile_name"
 }
