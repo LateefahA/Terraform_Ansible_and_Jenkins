aa887ec73ad516101546bdd9ac4f0c1b3ae98f91
Lateefat <arogundadelateefat7@gmail.com>
2024-01-09 14:47:51 +0000

    added EC2 wait

--- compute.tf
+++ compute.tf
@@ -80,6 +80,7 @@ output "grafana_access" {
   value = {for i in aws_instance.mtc_main[*] : i.tags.Name => "${i.public_ip}:3000"}
 }
 
-output "instance_ips" {
-  value = [for i in aws_instance.mtc_main[*]: i.public_ip]
+
+output "instance_ids" {
+  value = [for i in aws_instance.mtc_main[*]: i.id]
 }
